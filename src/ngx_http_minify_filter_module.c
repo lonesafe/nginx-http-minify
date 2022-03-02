27444880c56e7d6ef6ea50cba59349916992fe34
lonesafe <85195433@qq.com>
2022-03-02 16:58:29 +0800

    l

--- src/ngx_http_minify_filter_module.c
+++ src/ngx_http_minify_filter_module.c
@@ -15,6 +15,13 @@ typedef struct
     ngx_array_t *types_keys;
 } ngx_http_minify_conf_t;
 
+typedef struct
+{
+    u_int all_end;
+    u_char *body;
+    ngx_buf_t *new_b;
+} ngx_http_minify_filter_ctx_t;
+
 static ngx_str_t ngx_http_minify_default_types[] = {
     ngx_string("application/x-javascript"),
     ngx_string("application/javascript"),
@@ -42,8 +49,10 @@ static ngx_command_t ngx_http_minify_filter_commands[] = {
 
 static ngx_int_t ngx_http_minify_filter_init(ngx_conf_t *cf);
 static void *ngx_http_minify_create_conf(ngx_conf_t *cf);
-static char *ngx_http_minify_merge_conf(ngx_conf_t *cf, void *parent,
-                                        void *child);
+static char *ngx_http_minify_merge_conf(ngx_conf_t *cf, void *parent, void *child);
+static ngx_int_t ngx_http_minify_buf_in_memory(ngx_buf_t *buf, ngx_http_request_t *r, ngx_http_minify_filter_ctx_t *ctx);
+static u_char *strAddstr(ngx_http_request_t *r, u_char *str1, u_char *str2);
+static u_char *getChar(ngx_http_request_t *r, u_char *pos, u_char *last);
 
 static ngx_http_module_t ngx_http_minify_filter_module_ctx = {
     NULL,                        /* preconfiguration */
@@ -75,13 +84,25 @@ ngx_module_t ngx_http_minify_filter_module = {
 
 static ngx_http_output_header_filter_pt ngx_http_next_header_filter;
 static ngx_http_output_body_filter_pt ngx_http_next_body_filter;
-static ngx_int_t ngx_http_minify_buf(ngx_buf_t *buf, ngx_http_request_t *r,
-                                     ngx_open_file_info_t *of);
+// static ngx_int_t ngx_http_minify_buf(ngx_buf_t *buf, ngx_http_request_t *r, ngx_open_file_info_t *of);
 
 static ngx_int_t
 ngx_http_minify_header_filter(ngx_http_request_t *r)
 {
+    ngx_http_minify_filter_ctx_t *ctx;
     ngx_http_minify_conf_t *conf;
+    if (r->headers_out.status == NGX_HTTP_NOT_MODIFIED)
+    {
+        return ngx_http_next_header_filter(r);
+    }
+
+    ctx = ngx_http_get_module_ctx(r, ngx_http_minify_filter_module);
+
+    if (ctx)
+    {
+        ngx_http_set_ctx(r, NULL, ngx_http_minify_filter_module);
+        return ngx_http_next_header_filter(r);
+    }
 
     conf = ngx_http_get_module_loc_conf(r, ngx_http_minify_filter_module);
 
@@ -89,26 +110,38 @@ ngx_http_minify_header_filter(ngx_http_request_t *r)
     {
         return ngx_http_next_header_filter(r);
     }
+    ctx = ngx_pcalloc(r->pool, sizeof(ngx_http_minify_filter_ctx_t));
+    if (ctx == NULL)
+    {
+        return NGX_ERROR;
+    }
+
+    ngx_http_set_ctx(r, ctx, ngx_http_minify_filter_module);
 
     ngx_http_clear_content_length(r);
     return ngx_http_next_header_filter(r);
 }
 
-static ngx_int_t ngx_http_minify_buf_in_memory(ngx_buf_t *buf, ngx_http_request_t *r);
-
 static ngx_int_t
 ngx_http_minify_body_filter(ngx_http_request_t *r, ngx_chain_t *in)
 {
-    ngx_buf_t *b;
-    ngx_int_t rc;
-    ngx_str_t *filename;
-    ngx_uint_t level;
+    // ngx_int_t rc;
+    // ngx_str_t *filename;
+    // ngx_uint_t level;
     ngx_chain_t *cl;
-    ngx_open_file_info_t of;
+    // ngx_open_file_info_t of;
     ngx_http_minify_conf_t *conf;
-    ngx_http_core_loc_conf_t *ccf;
+    // ngx_http_core_loc_conf_t *ccf;
+    ngx_http_minify_filter_ctx_t *ctx;
 
-    ccf = ngx_http_get_module_loc_conf(r, ngx_http_core_module);
+    ctx = ngx_http_get_module_ctx(r, ngx_http_minify_filter_module);
+
+    if (ctx == NULL)
+    {
+        return ngx_http_next_body_filter(r, in);
+    }
+
+    // ccf = ngx_http_get_module_loc_conf(r, ngx_http_core_module);
     conf = ngx_http_get_module_loc_conf(r, ngx_http_minify_filter_module);
     if (!conf->enable)
     {
@@ -122,92 +155,162 @@ ngx_http_minify_body_filter(ngx_http_request_t *r, ngx_chain_t *in)
 
     ngx_log_debug0(NGX_LOG_DEBUG_HTTP, r->connection->log, 0,
                    "http minify filter");
-
+    ngx_log_error(NGX_LOG_CRIT, r->connection->log, 0, "进来");
     for (cl = in; cl; cl = cl->next)
     {
-        b = cl->buf;
+        ngx_log_error(NGX_LOG_CRIT, r->connection->log, 0, "是否是最后啦(%d)(%d)", cl->buf->last_buf, cl->buf->last_in_chain);
+        //判断是否最后了
+        ctx->all_end = cl->buf->last_buf;
+        ctx->new_b = cl->buf;
+        u_char *now_buffer = getChar(r, ctx->new_b->last, ctx->new_b->pos);
+        ngx_log_error(NGX_LOG_CRIT, r->connection->log, 0, "获取到newbuffer啦");
+        ctx->body = strAddstr(r, ctx->body, now_buffer);
+        ngx_log_error(NGX_LOG_CRIT, r->connection->log, 0, "组合body啦");
+        // b = cl->buf;
+        // if (cl->buf->in_file)
+        // {
+        //     ngx_memzero(&of, sizeof(ngx_open_file_info_t));
 
-        if (cl->buf->in_file)
-        {
-            ngx_memzero(&of, sizeof(ngx_open_file_info_t));
+        //     of.read_ahead = ccf->read_ahead;
+        //     of.directio = ccf->directio;
+        //     of.valid = ccf->open_file_cache_valid;
+        //     of.min_uses = ccf->open_file_cache_min_uses;
+        //     of.errors = ccf->open_file_cache_errors;
+        //     of.events = ccf->open_file_cache_events;
 
-            of.read_ahead = ccf->read_ahead;
-            of.directio = ccf->directio;
-            of.valid = ccf->open_file_cache_valid;
-            of.min_uses = ccf->open_file_cache_min_uses;
-            of.errors = ccf->open_file_cache_errors;
-            of.events = ccf->open_file_cache_events;
+        //     filename = &cl->buf->file->name;
+        //     if (ngx_open_cached_file(ccf->open_file_cache, filename, &of, r->pool) != NGX_OK)
+        //     {
+        //         switch (of.err)
+        //         {
 
-            filename = &cl->buf->file->name;
-            if (ngx_open_cached_file(ccf->open_file_cache, filename, &of, r->pool) != NGX_OK)
-            {
-                switch (of.err)
-                {
+        //         case 0:
+        //             return NGX_HTTP_INTERNAL_SERVER_ERROR;
 
-                case 0:
-                    return NGX_HTTP_INTERNAL_SERVER_ERROR;
+        //         case NGX_ENOENT:
+        //         case NGX_ENOTDIR:
+        //         case NGX_ENAMETOOLONG:
 
-                case NGX_ENOENT:
-                case NGX_ENOTDIR:
-                case NGX_ENAMETOOLONG:
+        //             level = NGX_LOG_ERR;
+        //             rc = NGX_HTTP_NOT_FOUND;
+        //             break;
 
-                    level = NGX_LOG_ERR;
-                    rc = NGX_HTTP_NOT_FOUND;
-                    break;
+        //         case NGX_EACCES:
 
-                case NGX_EACCES:
+        //             level = NGX_LOG_ERR;
+        //             rc = NGX_HTTP_FORBIDDEN;
+        //             break;
 
-                    level = NGX_LOG_ERR;
-                    rc = NGX_HTTP_FORBIDDEN;
-                    break;
+        //         default:
 
-                default:
+        //             level = NGX_LOG_CRIT;
+        //             rc = NGX_HTTP_INTERNAL_SERVER_ERROR;
+        //             break;
+        //         }
 
-                    level = NGX_LOG_CRIT;
-                    rc = NGX_HTTP_INTERNAL_SERVER_ERROR;
-                    break;
-                }
+        //         if (rc != NGX_HTTP_NOT_FOUND || ccf->log_not_found)
+        //         {
+        //             ngx_log_error(level, r->connection->log, of.err,
+        //                           "%s \"%V\" failed", of.failed, filename);
+        //         }
 
-                if (rc != NGX_HTTP_NOT_FOUND || ccf->log_not_found)
-                {
-                    ngx_log_error(level, r->connection->log, of.err,
-                                  "%s \"%V\" failed", of.failed, filename);
-                }
+        //         return rc;
+        //     }
 
-                return rc;
-            }
+        //     if (!of.is_file)
+        //     {
+        //         ngx_log_error(NGX_LOG_CRIT, r->connection->log, ngx_errno,
+        //                       "\"%V\" is not a regular file", filename);
+        //         return NGX_HTTP_NOT_FOUND;
+        //     }
 
-            if (!of.is_file)
-            {
-                ngx_log_error(NGX_LOG_CRIT, r->connection->log, ngx_errno,
-                              "\"%V\" is not a regular file", filename);
-                return NGX_HTTP_NOT_FOUND;
-            }
+        //     if (of.size == 0)
+        //     {
+        //         continue;
+        //     }
 
-            if (of.size == 0)
-            {
-                continue;
-            }
+        //     ngx_http_minify_buf(b, r, &of);
+        // }
+        // else
+        // {
 
-            ngx_http_minify_buf(b, r, &of);
-        }
-        else
-        {
+        // if (b->pos == NULL)
+        // {
+        //     continue;
+        // }
 
-            if (b->pos == NULL)
-            {
-                continue;
-            }
-
-            ngx_http_minify_buf_in_memory(b, r);
-        }
+        // ngx_http_minify_buf_in_memory(b, r, ctx);
+        // }
     }
+    if (ctx->all_end)
+    {
+        ngx_buf_t *out = ngx_create_temp_buf(r->pool, ngx_strlen(ctx->body));
+        out->pos = ctx->body;
+        out->end = out->pos;
+        out->last = out->pos + ngx_strlen(ctx->body);
+        in->buf = out;
+        in->next = NULL;
+        ngx_http_minify_buf_in_memory(ctx->new_b, r, ctx);
+        return ngx_http_next_body_filter(r, in);
+    }
+    else
+    {
+        return NGX_OK;
+    }
+}
 
-    return ngx_http_next_body_filter(r, in);
+/**
+ * @brief 获取一段char
+ *
+ * @param pos
+ * @param last
+ * @return u_char*
+ */
+static u_char *getChar(ngx_http_request_t *r, u_char *pos, u_char *last)
+{
+    u_char *e = (u_char *)ngx_pcalloc(r->pool, pos - last);
+    ngx_log_error(NGX_LOG_CRIT, r->connection->log, 0, "内存分配啦");
+
+    int i = 0;
+    for (;;)
+    {
+        ngx_log_error(NGX_LOG_CRIT, r->connection->log, 0, "循环开始啦getChar");
+
+        if (pos >= last)
+        {
+            break;
+        }
+        u_char c = pos[0];
+        ++pos;
+        e[i] = c;
+        i++;
+    }
+    ngx_log_error(NGX_LOG_CRIT, r->connection->log, 0, "信息返回啦");
+
+    return e;
+}
+
+/**
+ * @brief 拼接两个char
+ *
+ * @param str1
+ * @param str2
+ * @return u_char*
+ */
+static u_char *strAddstr(ngx_http_request_t *r, u_char *str1, u_char *str2)
+{
+    ngx_log_error(NGX_LOG_CRIT, r->connection->log, 0, "开始拼接啦(%d)(%d)", str1, str2);
+    char *str1_ = (char *)str1;
+    const char *str2_ = (const char *)str2;
+    ngx_log_error(NGX_LOG_CRIT, r->connection->log, 0, "两个char都转换完了(%d)(%d)", str1_, str2_);
+    strcat(str1_, str2_);
+    ngx_log_error(NGX_LOG_CRIT, r->connection->log, 0, "拼接成功啦(%d)(%d)", str1_, str2_);
+
+    return (u_char *)str1_;
 }
 
 static ngx_int_t
-ngx_http_minify_buf_in_memory(ngx_buf_t *buf, ngx_http_request_t *r)
+ngx_http_minify_buf_in_memory(ngx_buf_t *buf, ngx_http_request_t *r, ngx_http_minify_filter_ctx_t *ctx)
 {
     ngx_buf_t *b = NULL, *dst = NULL, *min_dst = NULL;
     ngx_int_t size;
@@ -256,105 +359,105 @@ ngx_http_minify_buf_in_memory(ngx_buf_t *buf, ngx_http_request_t *r)
     return NGX_OK;
 }
 
-static ngx_int_t
-ngx_http_minify_buf(ngx_buf_t *buf, ngx_http_request_t *r,
-                    ngx_open_file_info_t *of)
-{
-    ngx_buf_t *b = NULL, *dst = NULL, *min_dst = NULL;
-    ngx_int_t size;
-    ngx_file_t *src_file;
+// static ngx_int_t
+// ngx_http_minify_buf(ngx_buf_t *buf, ngx_http_request_t *r,
+//                     ngx_open_file_info_t *of)
+// {
+//     ngx_buf_t *b = NULL, *dst = NULL, *min_dst = NULL;
+//     ngx_int_t size;
+//     ngx_file_t *src_file;
 
-    src_file = ngx_pcalloc(r->pool, sizeof(ngx_file_t));
-    if (src_file == NULL)
-    {
-        return NGX_HTTP_INTERNAL_SERVER_ERROR;
-    }
+//     src_file = ngx_pcalloc(r->pool, sizeof(ngx_file_t));
+//     if (src_file == NULL)
+//     {
+//         return NGX_HTTP_INTERNAL_SERVER_ERROR;
+//     }
 
-    src_file->fd = of->fd;
-    src_file->name = buf->file->name;
-    src_file->log = r->connection->log;
-    src_file->directio = of->is_directio;
+//     src_file->fd = of->fd;
+//     src_file->name = buf->file->name;
+//     src_file->log = r->connection->log;
+//     src_file->directio = of->is_directio;
 
-    size = of->size;
+//     size = of->size;
 
-    b = ngx_calloc_buf(r->pool);
-    if (b == NULL)
-    {
-        return NGX_HTTP_INTERNAL_SERVER_ERROR;
-    }
+//     b = ngx_calloc_buf(r->pool);
+//     if (b == NULL)
+//     {
+//         return NGX_HTTP_INTERNAL_SERVER_ERROR;
+//     }
 
-    b->start = ngx_palloc(r->pool, size + 1);
-    b->pos = b->start;
-    b->last = b->start;
-    b->end = b->last + size;
-    b->temporary = 1;
+//     b->start = ngx_palloc(r->pool, size + 1);
+//     b->pos = b->start;
+//     b->last = b->start;
+//     b->end = b->last + size;
+//     b->temporary = 1;
 
-    dst = b;
+//     dst = b;
 
-#if (NGX_HAVE_FILE_AIO)
-    ssize_t n;
-    ngx_output_chain_ctx_t *ctx;
-    ctx = ngx_http_get_module_ctx(r, ngx_http_minify_filter_module);
-    if (ctx->aio_handler)
-    {
-        n = ngx_file_aio_read(src_file, dst->pos, (size_t)size, 0,
-                              ctx->pool);
+// #if (NGX_HAVE_FILE_AIO)
+//     ssize_t n;
+//     ngx_output_chain_ctx_t *ctx;
+//     ctx = ngx_http_get_module_ctx(r, ngx_http_minify_filter_module);
+//     if (ctx->aio_handler)
+//     {
+//         n = ngx_file_aio_read(src_file, dst->pos, (size_t)size, 0,
+//                               ctx->pool);
 
-        if (n == NGX_AGAIN)
-        {
-            ctx->aio_handler(ctx, src_file);
-            return NGX_AGAIN;
-        }
-    }
-    else
-    {
-        n = ngx_read_file(src_file, dst->pos, (size_t)size, 0);
-    }
-#else
+//         if (n == NGX_AGAIN)
+//         {
+//             ctx->aio_handler(ctx, src_file);
+//             return NGX_AGAIN;
+//         }
+//     }
+//     else
+//     {
+//         n = ngx_read_file(src_file, dst->pos, (size_t)size, 0);
+//     }
+// #else
 
-    ngx_read_file(src_file, dst->pos, (size_t)size, 0);
-#endif
+//     ngx_read_file(src_file, dst->pos, (size_t)size, 0);
+// #endif
 
-    dst->end[0] = 0;
+//     dst->end[0] = 0;
 
-    b = ngx_calloc_buf(r->pool);
-    if (b == NULL)
-    {
-        return NGX_HTTP_INTERNAL_SERVER_ERROR;
-    }
+//     b = ngx_calloc_buf(r->pool);
+//     if (b == NULL)
+//     {
+//         return NGX_HTTP_INTERNAL_SERVER_ERROR;
+//     }
 
-    b->start = ngx_palloc(r->pool, size);
-    b->pos = b->start;
-    b->last = b->start;
-    b->end = b->last + size;
-    b->temporary = 1;
+//     b->start = ngx_palloc(r->pool, size);
+//     b->pos = b->start;
+//     b->last = b->start;
+//     b->end = b->last + size;
+//     b->temporary = 1;
 
-    min_dst = b;
-    const char *header_content_type = (const char *)r->headers_out.content_type.data;
-    if (strstr(header_content_type, (const char *)ngx_http_minify_default_types[0].data) != 0 ||
-        strstr(header_content_type, (const char *)ngx_http_minify_default_types[1].data) != 0 ||
-        strstr(header_content_type, (const char *)ngx_http_minify_default_types[2].data) != 0)
-    {
-        jsmin(dst, min_dst);
-    }
-    else if (strstr(header_content_type, (const char *)ngx_http_minify_default_types[3].data) != 0)
-    {
-        cssmin(dst, min_dst);
-    }
-    else
-    {
-        return NGX_HTTP_INTERNAL_SERVER_ERROR;
-    }
+//     min_dst = b;
+//     const char *header_content_type = (const char *)r->headers_out.content_type.data;
+//     if (strstr(header_content_type, (const char *)ngx_http_minify_default_types[0].data) != 0 ||
+//         strstr(header_content_type, (const char *)ngx_http_minify_default_types[1].data) != 0 ||
+//         strstr(header_content_type, (const char *)ngx_http_minify_default_types[2].data) != 0)
+//     {
+//         jsmin(dst, min_dst);
+//     }
+//     else if (strstr(header_content_type, (const char *)ngx_http_minify_default_types[3].data) != 0)
+//     {
+//         cssmin(dst, min_dst);
+//     }
+//     else
+//     {
+//         return NGX_HTTP_INTERNAL_SERVER_ERROR;
+//     }
 
-    buf->start = min_dst->start;
-    buf->pos = min_dst->pos;
-    buf->last = min_dst->last;
-    buf->end = min_dst->end;
-    buf->memory = 1;
-    buf->in_file = 0;
+//     buf->start = min_dst->start;
+//     buf->pos = min_dst->pos;
+//     buf->last = min_dst->last;
+//     buf->end = min_dst->end;
+//     buf->memory = 1;
+//     buf->in_file = 0;
 
-    return NGX_OK;
-}
+//     return NGX_OK;
+// }
 
 static void *
 ngx_http_minify_create_conf(ngx_conf_t *cf)
